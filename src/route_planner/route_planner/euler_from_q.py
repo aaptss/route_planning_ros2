3bb182431cb07bf62e225cdc2dbc21e8f2e607aa
aaptss <armanpts@outlook.com>
2021-01-28 01:16:06 +0300

    initial commit

---
+++ src/route_planner/route_planner/euler_from_q.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import math 
+
+def eu_from_q(x,y,z,w):
+    num0 = +2 * (w * x + y * z)
+    den0 = +1 - 2*(x * x + y * y)
+    roll = math.atan2(num0, den0)
+
+    num1 = +2 * (w * y - z * x)
+    if num1 > +1:
+        num1 = +1
+    elif num1 < -1:
+        num1 = -1
+    else:
+        pass
+    pitch = math.asin(num1)
+
+    num2 = +2 * (w * z + x* y)
+    den2 = +1 - 2 * (y * y + z * z)
+    yaw = math.atan2(num2,den2)
+
+    return roll, pitch, yaw
