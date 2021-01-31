ed334f38abd5736666fe6c0999ca243a0f96f607
aaptss <armanpts@outlook.com>
2021-01-31 06:08:34 +0300

    random points now chosen efficiently

--- src/route_planner/route_planner/terminal_pts_fetcher.py
+++ src/route_planner/route_planner/terminal_pts_fetcher.py
@@ -17,11 +17,12 @@ class PintFetcherNode(Node):
         self.y_start = 0.0
         self.x_end = 0.0
         self.y_end = 0.0
-        self.isMap = False
         self.noInputFlag = False
+        self.isMapFlag = False
+        self.isGoodPtArrayRdy = False
         self.point_id = 0
         self.rs = RoboState()
-
+        self.timer_ = self.create_timer(3, self.publish_both)
         self.start_publisher_ = self.create_publisher(
             PoseStamped,
             "start",
@@ -37,93 +38,52 @@ class PintFetcherNode(Node):
             1)
 
     def new_map_cb(self, msg):
-        # self.get_logger().info(msg.header.frame_id + " map frame received")
-        # self.get_logger().info("    " + self.map.header.frame_id + " was previous")
-
-        if self.map.header.frame_id != msg.header.frame_id:
-            self.isMap = True
+        self.isMapFlag = True
         self.map = msg
-
-        if (self.noInputFlag == False): # see fetch_points()
-        # x_start == 1024 means keyboard input turned off, the points gonna go random.
-            while True:
-                self.fetch_points()
-                if self.x_start == 1024.0:
-                    break
-                if self.arePointsOk():
-                    break
-        else:
-            self.noInputFlag = True
-            self.get_random_points()
-
-        if self.isMap and self.arePointsOk():
-            self.isMap = False
-
-        if self.arePointsOk():
-            self.point_id = self.point_id + 1
-            self.publish_start()
-            self.get_logger().info("start_pt(" + str(self.x_start)+", " + str(self.y_start) + ") published")
-            self.publish_end()
-            self.get_logger().info("end_pt(" + str(self.x_end)+", " + str(self.y_end) + ") published")
-
-    def publish_start(self):
-        start_pt = PoseStamped()
-        if self.x_start == 1024:
-            start_pt.pose.position.x = self.buf[0]
-            start_pt.pose.position.y = self.buf[1]
-        else:
-            start_pt.pose.position.x = self.x_start
-            start_pt.pose.position.y = self.y_start
-        start_pt.pose.position.z = 0.1
-        start_pt.header.frame_id = hex(self.point_id)
-        self.start_publisher_.publish(start_pt)
-
-    def publish_end(self):
-        end_pt = PoseStamped()
-        if self.x_start == 1024:
-            end_pt.pose.position.x = self.buf[2]
-            end_pt.pose.position.y = self.buf[3]
-        else:
-            end_pt.pose.position.x = self.x_end
-            end_pt.pose.position.y = self.y_end
-        end_pt.pose.position.z = 0.1
-        end_pt.header.frame_id = hex(self.point_id)
-
-        self.end_publisher_.publish(end_pt)
 
     def fetch_points(self):
         self.get_logger().info("If you want to turn off keybard input and take random pts, type in 1024")
         self.buf = [self.x_start, self.y_start, self.x_end, self.y_end]
-        self.x_start = float(input("Please enter the starting X coordinate: "))
-        self.y_start = float(input("Please enter the starting Y coordinate: "))
-        self.get_logger().info("Your input is (X, Y) = (" +
+        self.x_start = float(input("Please enter the start X coordinate: "))
+        self.y_start = float(input("Please enter the start Y coordinate: "))
+        self.get_logger().info("Your start input is (X, Y) = (" +
             str(self.x_start)+", " + str(self.y_start) + ")\r\n")
 
-        self.x_end = float(input("Please enter the terminal X coordinate: "))
-        self.y_end = float(input("Please enter the terminal Y coordinate: "))
+        self.x_end = float(input("Please enter the end X coordinate: "))
+        self.y_end = float(input("Please enter the end Y coordinate: "))
 
-        self.get_logger().info("Your input is (X, Y) = (" +
+        self.get_logger().info("Your end input is (X, Y) = (" +
             str(self.x_end)+", " + str(self.y_end) + ")\r\n")
 
+        # x_start == 1024 means keyboard input turned off, the points gonna go random.
         if self.x_start == 1024.0:
             self.noInputFlag = True
-
-        self.x_start = self.my_rounding(self.x_start, self.map.info.resolution)
-        self.y_start = self.my_rounding(self.y_start, self.map.info.resolution)
-        self.x_end = self.my_rounding(self.x_end, self.map.info.resolution)
-        self.y_end = self.my_rounding(self.y_end, self.map.info.resolution)
+        else:
+            self.x_start = self.my_rounding(self.x_start, self.map.info.resolution)
+            self.y_start = self.my_rounding(self.y_start, self.map.info.resolution)
+            self.x_end = self.my_rounding(self.x_end, self.map.info.resolution)
+            self.y_end = self.my_rounding(self.y_end, self.map.info.resolution)
 
     def get_random_points(self):
-        while True:
-            ranstart = random.choice(list(enumerate(self.map.data)))
-            ranend = random.choice(list(enumerate(self.map.data)))
-            if ((ranstart[1] == 0) and(ranend[1] == 0)):
-                break
+        # while True:
+        #     ranstart = random.choice(list(enumerate(self.map.data)))
+        #     ranend = random.choice(list(enumerate(self.map.data)))
+        #     if ((ranstart[1] == 0) and(ranend[1] == 0)):
+        #         break
+        if not self.isGoodPtArrayRdy:
+            good_points = [x==0 for x in self.map.data] # data(data)
+            good_points = np.where(good_points)[0]
+        ranstart = random.choice(good_points) # choose an index of a random free point 
+        ranend = random.choice(good_points)
 
-        start_x_id = ranstart[0] % self.map.info.width # pixel index by axis from 1D occupancy grid 
-        start_y_id = ranstart[0] // self.map.info.width # pixel index by axis from 1D occupancy grid 
-        end_x_id =  ranend[0] % self.map.info.width # pixel index by axis from 1D occupancy grid 
-        end_y_id = ranend[0] // self.map.info.width # pixel index by axis from 1D occupancy grid 
+        # start_x_id = ranstart[0] % self.map.info.width # use with while-do loop 
+        # start_y_id = ranstart[0] // self.map.info.width # use with while-do loop 
+        # end_x_id =  ranend[0] % self.map.info.width # use with while-do loop 
+        # end_y_id = ranend[0] // self.map.info.width # use with while-do loop 
+        start_x_id = ranstart % self.map.info.width # pixel index by axis from 1D occupancy grid 
+        start_y_id = ranstart // self.map.info.width # pixel index by axis from 1D occupancy grid 
+        end_x_id =  ranend % self.map.info.width # pixel index by axis from 1D occupancy grid 
+        end_y_id = ranend // self.map.info.width # pixel index by axis from 1D occupancy grid 
 
         self.x_start = start_x_id * self.map.info.resolution + self.map.info.origin.position.x # coordinate from pixel index
         self.y_start = start_y_id * self.map.info.resolution + self.map.info.origin.position.y # coordinate from pixel index
@@ -135,19 +95,16 @@ class PintFetcherNode(Node):
         self.x_end = self.my_rounding(self.x_end,self.map.info.resolution)
         self.y_end = self.my_rounding(self.y_end,self.map.info.resolution)
 
-    def arePointsOk(self):
-        # TODO: make an util.py with this and use this method across the workspace to avoid copypase
-        return (self.isPointInBoundaries(self.x_start, self.y_start) and
-            self.isPointInBoundaries(self.x_end, self.y_end) and
-            self.isPointFree(self.x_start, self.y_start) and
-            self.isPointFree(self.x_end, self.y_end))
-
     def isPointInBoundaries(self, x, y):
         self.xmin = self.map.info.origin.position.x
         self.ymin = self.map.info.origin.position.y
         self.xmax = round(self.xmin + self.map.info.width * self.map.info.resolution, 3) 
         self.ymax = round(self.ymin + self.map.info.height * self.map.info.resolution, 3)
-        return (x < self.xmax) and (x > self.xmin) and (y < self.ymax) and (y > self.ymin)
+        
+        return ((x < (self.xmax - self.rs.diam)) 
+        and (x > self.xmin) 
+        and (y < (self.ymax - self.rs.diam)) 
+        and (y > self.ymin))
 
     def isPointFree(self, x, y):
         res = self.map.info.resolution
@@ -160,9 +117,68 @@ class PintFetcherNode(Node):
                 flag[row][col] = (self.map.data[n + col + int(self.map.info.width) * row] == 0)
         return flag.all() # check if all the footprint of the robot is in the free space
 
+    def arePointsOk(self):
+        # TODO: make an util.py with this and use this method across the workspace to avoid copypase
+        return (self.isPointInBoundaries(self.x_start, self.y_start) and
+            self.isPointInBoundaries(self.x_end, self.y_end) and
+            self.isPointFree(self.x_start, self.y_start) and
+            self.isPointFree(self.x_end, self.y_end))
+
+    def publish_start(self):
+        start_pt = PoseStamped()
+        if self.x_start == 1024:
+            start_pt.pose.position.x = self.buf[0]
+            start_pt.pose.position.y = self.buf[1]
+        else:
+            start_pt.pose.position.x = self.x_start
+            start_pt.pose.position.y = self.y_start
+        start_pt.pose.position.z = 0.1
+        start_pt.header.frame_id = hex(self.point_id)
+        self.start_publisher_.publish(start_pt)
+        self.get_logger().info("start_pt(" + str(self.x_start)+", " + str(self.y_start) + ") published")
+
+    def publish_end(self):
+        end_pt = PoseStamped()
+        if self.x_start == 1024:
+            end_pt.pose.position.x = self.buf[2]
+            end_pt.pose.position.y = self.buf[3]
+        else:
+            end_pt.pose.position.x = self.x_end
+            end_pt.pose.position.y = self.y_end
+        end_pt.pose.position.z = 0.1
+        end_pt.header.frame_id = hex(self.point_id)
+        self.end_publisher_.publish(end_pt)
+        self.get_logger().info("end_pt(" + str(self.x_end)+", " + str(self.y_end) + ") published")
+
+    def publish_both(self):
+        if self.isMapFlag:
+            if not self.noInputFlag: # see fetch_points()
+                while True:
+                    self.fetch_points()
+                    if self.noInputFlag:
+                        self.get_logger().info("go for random")
+                        self.get_random_points()
+                        break
+                    if self.arePointsOk():
+                        break
+            else:
+                self.get_logger().info("go for random")
+                self.noInputFlag = True
+                self.get_random_points()
+
+            if self.arePointsOk():
+                self.get_logger().info("points are ok!")
+                self.get_logger().info("prepping to publish!")
+                self.point_id = self.point_id + 1
+                self.publish_start()
+                self.publish_end()
+            else:
+                self.get_logger().info("points not okay, need new pair!")
+                pass
+
     @staticmethod
     def my_rounding(a,b):
-        # get clean division when perform operations with the resolution of the map
+        # get clean coordinate value inline with map's resolution
         # inner round() to actually round the valuse
         # the second round is needed to get rid of float poind division imperfections
         return round(round(a/b)*b,2)
